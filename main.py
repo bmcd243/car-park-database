b384c14d807024a7851f32ff3c0f985ba5995865
bmcd243 <benmcdowelluk@gmail.com>
2021-01-21 13:18:27 +0000

    grid working

--- main.py
+++ main.py
@@ -7,39 +7,87 @@ import tkinter.font as tkFont
 from PIL import ImageTk, Image
 
 root = Tk()
-root.geometry("800x1000")
+root.geometry("1000x800")
 root.title("Collyer's Car Park")
 
 def restart():
 	os.execl(sys.executable, sys.executable, *sys.argv)
 
-def staff():
+def photo_display():
+	image_1 = ImageTk.PhotoImage(Image.open("./images/lambo.jpg"))
+	photo = Label(main_frame, image = image_1)
+	photo.image = image_1
+	photo.grid(row=6, column=4, sticky=E, pady=2)
+
+def display():
+
+	photo_display()
+
+	select_text = Label(main_frame, text="Profession")
+	make_text = Label(main_frame, text="Make")
+	model_text = Label(main_frame, text="Model")
+	colour_text = Label(main_frame, text="Colour")
+	reg_text = Label(main_frame, text="Registration")
+
+	select_text.grid(row = 1, column = 0, sticky = W, pady = 2)
+	make_text.grid(row = 2, column = 0, sticky = W, pady = 2)
+	model_text.grid(row = 3, column = 0, sticky = W, pady = 2)
+	colour_text.grid(row = 4, column = 0, sticky = W, pady = 2)
+	reg_text.grid(row = 5, column = 0, sticky = W, pady = 2)
+
+
+
+	dropdown_options = [
+		"Staff",
+		"Student",
+		"Visitor"
+	]
+	variable = StringVar(main_frame)
+	variable.set(dropdown_options[0])
+
+	dropdown = OptionMenu(main_frame, variable, *dropdown_options)
+	dropdown.grid(row=1, column=1, pady=2)
+
+	make_entry = Entry(main_frame)
+	make_entry.grid(row=2, column=1, pady=2)
+
+	model_entry = Entry(main_frame)
+	model_entry.grid(row=3, column=1)
+
+	colour_entry = Entry(main_frame)
+	colour_entry.grid(row=4, column=1)
+
+	reg_entry = Entry(main_frame)
+	reg_entry.grid(row=5, column=1)
+
+	def fetch_details():
+		profession = variable.get()
+		make = make_entry.get()
+		model = model_entry.get()
+		colour = colour_entry.get()
+		reg = reg_entry.get()
+
+		to_database(profession, make, model, colour, reg)
+
+	confirm_button = ttk.Button(main_frame, text="Confirm", command= fetch_details)
+	confirm_button.grid(row=6, column=1)
+
+def to_database(profession, make, model, colour, reg):
 	print("")
 
-def visitor():
-	print("")
-
-def student():
-	print("")
-
-welcome = Label(root, text="Hello, please use the menu above to navigate the interface")
-welcome.pack()
-
-### create menu bar
-menu_bar = Menu(root)
-root.config(menu=menu_bar)
-
-menu_1 = Menu(menu_bar)
-menu_bar.add_cascade(label="Select who you are", menu=menu_1)
-menu_1.add_command(label="Caeser Cipher", command=staff)
-menu_1.add_command(label="Vernam Cipher", command=student)
-menu_1.add_command(label="Symmetric and Asymmetric encryption", command=visitor)
-menu_1.add_command(label="Restart Program", command=restart)
 
 
-staff_frame = Frame(root, width=1000, height=800)
-visitor_frame = Frame(root, width=1000, height=800)
-student_frame = Frame(root, width=1000, height=800)
+
+
+
+
+main_frame = Frame(root, width=1000, height=800)
+main_frame.grid()
+
+welcome = Label(main_frame, text="Hello, please use the menu above to navigate the interface")
+welcome.grid(row=0, column=3)
+
+display()
 
 
 root.mainloop()
